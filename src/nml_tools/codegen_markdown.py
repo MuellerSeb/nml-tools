3619349a6a5b1bfac9f24030106ef60f6d4a58a7
Sebastian Müller <mueller.seb@posteo.de>
2026-01-23 15:27:31 +0100

    lint

--- src/nml_tools/codegen_markdown.py
+++ src/nml_tools/codegen_markdown.py
@@ -2,10 +2,10 @@
 
 from __future__ import annotations
 
-from pathlib import Path
-from typing import Any
 import re
 import string
+from pathlib import Path
+from typing import Any
 
 from .codegen_fortran import (
     FieldTypeInfo,
